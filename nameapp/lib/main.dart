813db7890fd49518b10441698db98e0dbfa92009
Cynthia-Isutsa <isutsacynthia@gmail.com>
2024-12-05 16:05:21 +0300

    app's look && functionality

--- nameapp/lib/main.dart
+++ nameapp/lib/main.dart
@@ -27,32 +27,103 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
-   void getNext() {
+  void getNext() {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
+
+
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    // requests access to the current state
     var pair = appState.current;
+     IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random awesome idea:'),
-          Text(pair.asPascalCase),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Text('A random awesome idea:'),
+            BigCard(pair: pair),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                    onPressed: () {
+                      appState.toggleFavorite();
+                      // print('button pressed!');
+                    },
+                   icon: Icon(icon),
+                   label: Text('Like'),
+                    ),
 
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-              // print('button pressed!');
-            },
-            child: Text('Next'),
-          ),
-        ],
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                    // print('button pressed!');
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: Text('Next'),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context); // requests access to the current theme
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onSecondary,
+    );
+
+    return Card(
+      color: theme.colorScheme
+          .secondary, // defines the card color to be the same as the the
+      // theme's color scheme property. The color scheme may contain many colors so the primary is the most prominent
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: pair.asPascalCase,
+        ),
       ),
     );
   }
